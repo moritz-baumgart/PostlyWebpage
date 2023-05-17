89ae14f9526ebc5e5345b25bb2dc662bcae7e865
orangeUNNIX <baumgartmoritz@web.de>
2023-05-17 04:19:45 +0200

    Installed primeng and primeicons. Added a basic UI for the start page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: StartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { StartComponent } from './start/start.component';
+import { SkeletonModule } from 'primeng/skeleton';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    InputTextModule,
+    SkeletonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
